194fe2a9ece785786ff4a9a146a91e322b1c3050
TrungDN <trungdo1@tdt.asia>
2023-08-23 16:12:41 +0700

    Initial Commit

---
+++ RectangleProblem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RectangleProblem
+//
+//  Created by ECO0973-TRUNGDN on 23/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
